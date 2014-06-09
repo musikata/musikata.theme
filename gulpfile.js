10b61c2f9c27fc3aca04f7cbd8ffe39dfee8bee5
adorsk <adorsk@gmail.com>
2014-06-09 07:35:56 -0400

    tweaking gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,44 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var browserSync = require('browser-sync');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
+var plumber = require('gulp-plumber');
+var connect = require('connect');
+var http = require('http');
 
-gulp.task('sass', function(){
+var compileSass = function() {
   gulp.src('scss/app.scss')
+  .pipe(plumber())
   .pipe(sass({
     loadPath: ['bower_components/foundation/scss'],
+    quiet: true
   }))
-  .on('error', function(){})
-  .pipe(gulp.dest('css'));
+  .pipe(plumber.stop())
+  .pipe(gulp.dest('css'))
+}
+
+gulp.task('livereload', function() {
+
+  // Recompile sass on sass changes.
+  gulp.src('scss/**/*.scss')
+  .pipe(watch(function(){
+    compileSass();
+  }));
+
+  // Do livereload on css change.
+  gulp.src('css/**/*.css')
+  .pipe(watch())
+  .pipe(livereload())
+  
 });
 
-gulp.task('browser-sync', function(){
-  browserSync.init(['css/*.css', 'js/*.js'], {
-    host: 'localhost',
-    server: {
-      baseDir: './'
-    }
-  });
+gulp.task('serve', function() {
+  var app = connect()
+  .use(connect.static(__dirname))
+
+  http.createServer(app).listen(3000)
+
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function(){
-  gulp.watch('scss/**/*.scss', ['sass']);
-});
+gulp.task('default', ['livereload', 'serve']);
 
